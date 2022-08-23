b475e93f5130de43e2e85832a32adce135981eb5
Frankley Omwenga <nchorefrankley27@gmail.com>
2022-08-22 17:38:48 -0700

    Print float

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,11 +6,16 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *current = list -> next;
-	listint_t *post_current = current -> next;
+	listint_t *slow_p = list, *fast_p = list;
 
-	if (current != NULL && post_current != NULL)
-		return (1);
-	else
-		return (0);
+	while (slow_p && fast_p && fast_p->next)
+	{
+		slow_p = slow_p->next;
+		fast_p = fast_p->next->next;
+		if (slow_p == fast_p)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
