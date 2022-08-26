25df022b80f5b97b4ef6c7372e9c47a05d21c3d7
Frankley Omwenga <nchorefrankley27@gmail.com>
2022-08-25 19:16:58 -0700

    import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,21 +3,25 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
 
-    if len(argv) != 4:
+    ac = len(argv) - 1
+
+    if ac != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    numb1 = int(argv[1])
-    numb2 = int(argv[3])
-
-    if argv[2] not in ["+", "-", "*", "/"]:
+    if argv[2] == '+':
+        sum = add(int(argv[1]), int(argv[3]))
+        print("{} {} {} = {:d}".format(argv[1], argv[2], argv[3], sum))
+    elif argv[2] == '-':
+        difr = sub(int(argv[1]), int(argv[3]))
+        print("{} {} {} = {:d}".format(argv[1], argv[2], argv[3], difr))
+    elif argv[2] == '*':
+        prod = mul(int(argv[1]), int(argv[3]))
+        print("{} {} {} = {:d}".format(argv[1], argv[2], argv[3], prod))
+    elif argv[2] == '/':
+        quot = div(int(argv[1]), int(argv[3]))
+        print("{} {} {} = {:d}".format(argv[1], argv[2], argv[3], quot))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    elif argv[2] is "+":
-        print("{} + {} = {}".format(numb1, numb2, add(numb1, numb2)))
-    elif argv[2] is "-":
-        print("{} - {} = {}".format(numb1, numb2, sub(numb1, numb2)))
-    elif argv[2] is "*":
-        print("{} * {} = {}".format(numb1, numb2, mul(numb1, numb2)))
-    elif argv[2] is "/":
-        print("{} / {} = {}".format(numb1, numb2, div(numb1, numb2))
+    exit(0)
--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-import printholberton
+__import__('sys').stdout.write('#pythoniscool\n')
