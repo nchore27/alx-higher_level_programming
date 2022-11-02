5f063b4173482d1f5300d9c8940ef858fcf32595
Frankley Omwenga <nchorefrankley27@gmail.com>
2022-11-01 19:11:45 -0700

    SQL - Introduction

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,7 +1,7 @@
 -- Insert a row in a specific database
--- INSERT INTO first_table (`id`, `name`) VALUES ("89", "Holberton");
+-- INSERT INTO first_table (`id`, `name`) VALUES ("89", "Best School");
 -- Inserts a new row in the table first_table
 INSERT INTO
     first_table ( `id`, `name` )
 VALUES
-    ( "89", "Holberton School" );
+    ( "89", "Best School" );
