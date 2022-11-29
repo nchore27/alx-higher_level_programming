b723ad630e1ba7e78960e5fd65847f094b958bda
nchore27 <nchorefrankley27@gmail.com>
2022-11-29 02:37:26 +0000

    java warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-/* Script that prints a message depending of the number of arguments passed */
-const argvLength = process.argv.length;
-if (argvLength === 2) console.log('No argument');
-else if (argvLength === 3) console.log('Argument found');
-else console.log('Arguments found');
+const args = process.argv.length;
+if (args === 2) {
+  console.log('No argument');
+} else if (args === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
