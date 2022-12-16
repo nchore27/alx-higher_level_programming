c667ddda8e6005b04dabaec848e2f11dc4ef5fab
nchore27 <nchorefrankley27@gmail.com>
2022-12-16 02:48:57 +0000

    Python - Network #0

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that takes in a url, sends a POST request to the passed URL and display the body of the response
-curl -s -X POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" $1
+# Sends a POST rqst and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
