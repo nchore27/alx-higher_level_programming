34c7e585000b63fd7e1e6120923ace8de3b58af9
nchore27 <omwengafrankley@gmail.com>
2024-02-13 08:19:57 +0000

    data structures

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,4 +1,5 @@
-)ce_in_list(my_list, idx, element):
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
     if 0 <= idx < len(my_list):
         my_list[idx] = element
     return(my_list)
