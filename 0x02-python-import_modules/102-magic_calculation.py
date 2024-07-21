4ea2b8f304364eb8e349ce00a1056f743b175929
nchore27 <nchorefrankley27@gmail.com>
2024-07-21 01:52:32 -0700

    python import modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-from sys import argv, exit
-import calculator_1
-if __name__ == "__main__":
-    if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
     else:
-        a, operator, b = argv[1:]
-        a = int(a)
-        b = int(b)
-        if operator == '+':
-            print(f"{a} + {b} = {calculator_1.add(a, b)}")
-        elif operator == '-':
-            print(f"{a} - {b} = {calculator_1.sub(a, b)}")
-        elif operator == '*':
-            print(f"{a} * {b} = {calculator_1.mul(a, b)}")
-        elif operator == '/':
-            print(f"{a} / {b} = {calculator_1.div(a, b)}")
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+        return (sub(a, b))
